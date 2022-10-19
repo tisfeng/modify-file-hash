bb0469803d5608442d8b47ccee09eca228a88052
tisfeng <tisfeng@gmail.com>
2022-10-20 00:13:35 +0800

    fix: restore file hash log is wrong

--- src/utils.tsx
+++ src/utils.tsx
@@ -2,7 +2,7 @@
  * @author: tisfeng
  * @createTime: 2022-10-19 22:28
  * @lastEditor: tisfeng
- * @lastEditTime: 2022-10-19 23:25
+ * @lastEditTime: 2022-10-20 00:11
  * @fileName: utils.tsx
  *
  * Copyright (c) 2022 by tisfeng, All Rights Reserved.
@@ -45,10 +45,10 @@ export default function ModifyHash(modify: boolean) {
   /**
    * Apppend a string to the end of all files in the current directory.
    */
-  function appendStringToFileRecursive(path: string, appendString = "#1024") {
+  function appendStringToFileRecursive(path: string, str: string) {
     const stat = fs.statSync(path);
     if (stat.isFile()) {
-      appendStringToFile(path, appendString);
+      appendStringToFile(path, str);
     } else if (stat.isDirectory()) {
       const subtitle = `## Directory: ${path} \n\n`;
       setMarkdown((prev) => prev + subtitle);
@@ -56,7 +56,7 @@ export default function ModifyHash(modify: boolean) {
       const files = fs.readdirSync(path);
       files.forEach((file) => {
         const filePath = path + "/" + file;
-        appendStringToFileRecursive(filePath, appendString);
+        appendStringToFileRecursive(filePath, str);
       });
     }
   }
@@ -64,30 +64,30 @@ export default function ModifyHash(modify: boolean) {
   /**
    * Apppend a string to the end of file.
    */
-  function appendStringToFile(filePath: string, appendString = "#1024") {
+  function appendStringToFile(filePath: string, str: string) {
     const stat = fs.statSync(filePath);
     if (stat.isFile()) {
       const oldMd5 = md5File(filePath);
       const oldMd5Log = `${path.basename(filePath)}, old md5: ${oldMd5}`;
       setMarkdown((prev) => prev + oldMd5Log + "\n\n");
 
-      fs.appendFileSync(filePath, appendString);
+      fs.appendFileSync(filePath, str);
 
       const newMd5 = md5File(filePath);
       const newMd5Log = `${path.basename(filePath)}, new md5: ${newMd5}`;
       setMarkdown((prev) => prev + newMd5Log + "\n\n");
     } else if (stat.isDirectory()) {
-      appendStringToFileRecursive(filePath, appendString);
+      appendStringToFileRecursive(filePath, str);
     }
   }
 
   /**
    * Remove the appendString in the last line of all files in the current directory.
    */
-  function removeStringFromFileRecursive(path: string, appendString = "#1024") {
+  function removeStringFromFileRecursive(path: string, str: string) {
     const stat = fs.statSync(path);
     if (stat.isFile()) {
-      removeStringFromFile(path, appendString);
+      removeStringFromFile(path, str);
     } else if (stat.isDirectory()) {
       const subtitle = `## Directory: ${path} \n\n`;
       setMarkdown((prev) => prev + subtitle);
@@ -95,27 +95,27 @@ export default function ModifyHash(modify: boolean) {
       const files = fs.readdirSync(path);
       files.forEach((file) => {
         const filePath = path + "/" + file;
-        removeStringFromFileRecursive(filePath, appendString);
+        removeStringFromFileRecursive(filePath, str);
       });
     }
   }
 
-  function removeStringFromFile(filePath: string, appendString = "#1024") {
+  function removeStringFromFile(filePath: string, str: string) {
     const stat = fs.statSync(filePath);
     if (stat.isFile()) {
-      const log = `restore ${path.basename(filePath)} hash, md5: ${md5File(filePath)}`;
-      setMarkdown((prev) => prev + log + "\n\n");
-
       const lines = fs.readFileSync(filePath, "utf-8").split("\n");
       const lastLine = lines.pop();
 
       // if last line contains appendString, remove it
-      if (lastLine?.includes(appendString)) {
-        const newLine = lastLine.replaceAll(appendString, "");
+      if (lastLine?.includes(str)) {
+        const newLine = lastLine.replaceAll(str, "");
 
         // write to file
         lines.push(newLine);
         fs.writeFileSync(filePath, lines.join("\n"));
+
+        const log = `restore ${path.basename(filePath)} hash, md5: ${md5File(filePath)}`;
+        setMarkdown((prev) => prev + log + "\n\n");
       }
     }
   }
@@ -127,9 +127,9 @@ export default function ModifyHash(modify: boolean) {
           paths.forEach((path) => {
             console.log(path.path);
             if (modify) {
-              appendStringToFileRecursive(path.path);
+              appendStringToFileRecursive(path.path, appendString);
             } else {
-              removeStringFromFileRecursive(path.path);
+              removeStringFromFileRecursive(path.path, appendString);
             }
             const completeLog = `## ${title} has been completed 🎉🎉🎉 \n\n`;
             setMarkdown((prev) => prev + completeLog);
